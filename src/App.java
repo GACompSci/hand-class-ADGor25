6ef9c8342e5fb94aa4c8276952ceddae79fcbb5d
Aidan Gor <agor25@germantownacademy.org>
2025-02-10 20:59:56 -0500

    Finished

--- src/App.java
+++ src/App.java
@@ -1,5 +1,33 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+        // Deck Assignment:
+        Deck deck = new Deck();
+        // System.out.println(deck.drawCard());
+        // System.out.println(deck.drawCard());
+        // System.out.println(deck);
+
+
+        // Hand Assignment:
+        Hand hand = new Hand(3, deck);
+
+        System.out.println("Original Hand:");
+        hand.printHand();
+        System.out.println("");
+
+        System.out.println("Sorted Hand:");
+        hand.sortHand();
+        hand.printHand();
+        System.out.println("");
+
+        System.out.println("Adding a Card:");
+        hand.addCard(new Card(3,2));
+        hand.printHand();
+        System.out.println("");
+
+        System.out.println("Removing that Card:");
+        hand.playCard(3, 2);
+        hand.printHand();
+
     }
 }
--- src/Hand.java
+++ src/Hand.java
@@ -1,3 +1,53 @@
+import java.util.ArrayList;
+
 public class Hand {
-    
+    ArrayList<Card> hand = new ArrayList<Card>();
+
+    public Hand(int handLength, Deck deck) {
+        for (int i = 0; i < handLength; i++) {
+            hand.add(deck.drawCard());
+        }
+    }
+
+    public int cardPresent(int value, int suit) {
+        for (Card card : hand) {
+            if (card.getValue() == value && card.getSuit() == suit) {
+                return hand.indexOf(card);
+            }
+        }
+        return -1;
+    }
+
+    public Card playCard(int value, int suit) {
+        int cardIndex = cardPresent(value, suit);
+        if (cardIndex != -1) {
+            Card drawnCard = hand.get(cardIndex);
+            hand.remove(drawnCard);
+            return drawnCard;
+        }
+        return null;
+    }
+
+    public void addCard(Card card) {
+        hand.add(card);
+    }
+
+    public void printHand() {
+        for (Card card : hand) {
+            System.out.println(card);
+        }
+    }
+
+    public void sortHand() {
+        for (int i = 0; i < hand.size() - 1; i++) {
+            for (int j = 0; j < hand.size() - i - 1; j++) {
+                Card temp = hand.get(j + 1);
+                Card below = hand.get(j);
+                if (below.getValue() > temp.getValue()) {
+                    hand.set(j + 1, below);
+                    hand.set(j, temp);
+                }
+            }
+        }
+    }
 }
